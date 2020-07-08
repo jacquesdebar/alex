8cfecf0890d72492ddc2d00e6054f5aa7c0251d9
jacquesdebar <jacques.debar@gmail.com>
2020-07-07 22:13:28 -0700

    fix noreferrer warning

--- src/components/layout.js
+++ src/components/layout.js
@@ -38,7 +38,7 @@ const Layout = ({ children }) => {
         <footer style={{position: `absolute`, left: `0`, bottom: `0`, width: `100%`}}>
           <hr style={{margin: `0 auto`, padding: `0`, height: `2px`, width: `100px`, backgroundColor: `#333`}}/>
           <div style={{display: `flex`, flexDirection: `row`,justifyContent: `center`, alignItems: `center`, height: `96px`, paddingBottom:`12px`}}>
-            <a href="https://www.instagram.com/rakisoundscape/" target="_blank">
+            <a href="https://www.instagram.com/rakisoundscape/" target="_blank" rel="noreferrer">
               <img src={raki} alt="raki" width="40px" style={{margin: `0`}} />
             </a>
           </div>
